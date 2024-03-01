1bd2f647fbc9bd14f681878e9bea2b0f7b74cd6e
JoanaLOliveira <joanalmarcolino@gmail.com>
2024-03-01 11:00:23 -0300

    continuação

---
+++ Abstratas/ControleSys/Veiculo.java
@@ -0,0 +1,9 @@
+package Abstratas.ControleSys;
+
+public abstract class Veiculo {
+    private int capacidade;
+    public Veiculo(int capacidade){
+        this.capacidade=capacidade;
+    }
+    public abstract void moverFrente();
+}
